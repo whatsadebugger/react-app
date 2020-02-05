36c97acc7edbb2a7609b8f41e9160caf18f57198
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2020-02-04 19:17:35 -0600

    need to bind to this before you can access this

--- src/App.js
+++ src/App.js
@@ -56,10 +56,33 @@ class App extends Component {
             </button>
           </div>
         )}
+        <ExplainBindingsComponent></ExplainBindingsComponent>
       </div>
     );
   }
 
+}
+
+class ExplainBindingsComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickMe = this.onClickMe.bind(this)
+  }
+
+  onClickMe() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <button
+        onClick={this.onClickMe}
+        type="button"
+      >
+        Click Me
+      </button>
+    );
+  }
 }
 
 export default App;
